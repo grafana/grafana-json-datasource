c1c27021e70cb7c20b7abbeac6f4c01545e6b5ce
Marcus Olsson <marcus.olsson@hey.com>
2021-02-01 13:52:38 +0100

    Fix nullable fields

--- src/datasource.ts
+++ src/datasource.ts
@@ -218,16 +218,16 @@ export const parseValues = (values: any[], type: FieldType): any[] => {
       // For time field, values are expected to be numbers representing a Unix
       // epoch in milliseconds.
 
-      if (values.every(value => typeof value === 'string')) {
-        return values.map(_ => (_ !== null ? parseISO(_).valueOf() : null));
+      if (values.filter(_ => _).every(value => typeof value === 'string')) {
+        return values.map(_ => (_ !== null ? parseISO(_).valueOf() : _));
       }
 
-      if (values.every(value => typeof value === 'number')) {
+      if (values.filter(_ => _).every(value => typeof value === 'number')) {
         const ms = 1_000_000_000_000;
 
         // If there are no "big" numbers, assume seconds.
-        if (values.every(_ => _ < ms)) {
-          return values.map(_ => _ * 1000.0);
+        if (values.filter(_ => _).every(_ => _ < ms)) {
+          return values.map(_ => (_ !== null ? _ * 1000.0 : _));
         }
 
         // ... otherwise assume milliseconds.
@@ -236,13 +236,17 @@ export const parseValues = (values: any[], type: FieldType): any[] => {
 
       throw new Error('Unsupported time property');
     case FieldType.string:
-      return values.every(_ => typeof _ === 'string') ? values : values.map(_ => _.toString());
+      return values.every(_ => typeof _ === 'string') ? values : values.map(_ => (_ !== null ? _.toString() : _));
     case FieldType.number:
-      return values.every(_ => typeof _ === 'number') ? values : values.map(_ => parseFloat(_));
+      return values.every(_ => typeof _ === 'number') ? values : values.map(_ => (_ !== null ? parseFloat(_) : _));
     case FieldType.boolean:
       return values.every(_ => typeof _ === 'boolean')
         ? values
         : values.map(_ => {
+            if (_ === null) {
+              return _;
+            }
+
             switch (_.toString()) {
               case '0':
               case 'false':
@@ -255,7 +259,7 @@ export const parseValues = (values: any[], type: FieldType): any[] => {
               case 'True':
                 return true;
               default:
-                throw new Error('Found non-boolean values in a field of type boolean');
+                throw new Error('Found non-boolean values in a field of type boolean: ' + _.toString());
             }
           });
     default:
--- src/parseValues.test.ts
+++ src/parseValues.test.ts
@@ -41,3 +41,32 @@ test('parse booleans from strings', () => {
   expect(parseValues(values, FieldType.string)).toStrictEqual(['false', 'true', 'false']);
   expect(parseValues(values, FieldType.time)).toStrictEqual([NaN, NaN, NaN]);
 });
+
+test('parse nullable strings', () => {
+  const values = ['foo', 'bar', null];
+
+  expect(() => parseValues(values, FieldType.boolean)).toThrow();
+  expect(parseValues(values, FieldType.number)).toStrictEqual([NaN, NaN, null]);
+  expect(parseValues(values, FieldType.string)).toStrictEqual(['foo', 'bar', null]);
+  expect(parseValues(values, FieldType.time)).toStrictEqual([NaN, NaN, null]);
+});
+
+test('parse nullable numbers', () => {
+  const values = [2005, null, 2006];
+
+  expect(() => parseValues(values, FieldType.boolean)).toThrow();
+  expect(parseValues(values, FieldType.number)).toStrictEqual([2005, null, 2006]);
+  expect(parseValues(values, FieldType.string)).toStrictEqual(['2005', null, '2006']);
+
+  // Numbers are assumed to be epoch time in seconds.
+  expect(parseValues(values, FieldType.time)).toStrictEqual([2005000, null, 2006000]);
+});
+
+test('parse nullable booleans', () => {
+  const values = [null, true, false];
+
+  expect(parseValues(values, FieldType.boolean)).toStrictEqual([null, true, false]);
+  expect(parseValues(values, FieldType.number)).toStrictEqual([null, NaN, NaN]);
+  expect(parseValues(values, FieldType.string)).toStrictEqual([null, 'true', 'false']);
+  expect(() => parseValues(values, FieldType.time)).toThrow();
+});
