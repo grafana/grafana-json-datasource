ec4229685d7281250c756f536c78d9aba4e91212
Jirka Fajfr <jirka.fajfr@enverus.com>
2021-05-17 11:03:40 -0500

    Adding `$__isoFrom()` and `$__isoTo()` macros

---
+++ src/datasource.test.ts
@@ -0,0 +1,24 @@
+import { replaceMacros } from './datasource';
+import { TimeRange, dateTime } from '@grafana/data';
+
+const sampleTimestampFrom = '2021-05-17T20:48:09.000Z'; // -> 1621284489
+const sampleTimestmapTo = '2021-05-17T20:50:23.000Z'; // -> 1621284623
+
+const range: TimeRange = {
+  from: dateTime(sampleTimestampFrom),
+  to: dateTime(sampleTimestmapTo),
+  raw: {
+    from: sampleTimestampFrom,
+    to: sampleTimestmapTo,
+  },
+};
+
+test('range gets converted into ISO8601 notation', () => {
+  expect(replaceMacros('$__isoFrom()', range)).toStrictEqual(sampleTimestampFrom);
+  expect(replaceMacros('$__isoTo()', range)).toStrictEqual(sampleTimestmapTo);
+});
+
+test('range gets converted into unix epoch notation', () => {
+  expect(replaceMacros('$__unixEpochFrom()', range)).toStrictEqual('1621284489');
+  expect(replaceMacros('$__unixEpochTo()', range)).toStrictEqual('1621284623');
+});
