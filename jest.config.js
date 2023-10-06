9e17ffff03a39b1cc46f86ab6115b82b1343945d
Gareth Dawson <gwdawson.work@gmail.com>
2023-10-06 15:12:18 +0100

    fix failing ci

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const { grafanaESModules, nodeModulesToTransform } = require('./.config/jest/utils');
+
 // force timezone to UTC to allow tests to work regardless of local timezone
 // generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
