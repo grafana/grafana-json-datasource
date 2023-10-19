f31253b729712541f7d00ac94a6270e901d5fc04
Zoltán Bedi <zoltan.bedi@gmail.com>
2023-10-19 16:19:45 +0200

    Fix test

--- src/datasource.test.ts
+++ src/datasource.test.ts
@@ -35,15 +35,15 @@ describe('datasource', () => {
   it('should throw error when method is not POST or GET', async () => {
     const ds = new JsonDataSource({ url: 'http://localhost:3000', jsonData: {} } as any);
 
-    const responsePUT = ds.doRequest({ method: 'PUT' } as any);
+    const responsePUT = ds.doRequest({ urlPath: 'http://localhost:3000/', method: 'PUT' } as any);
 
     expect(responsePUT).rejects.toThrowError('Invalid method PUT');
 
-    const responsePATCH = ds.doRequest({ method: 'PATCH' } as any);
+    const responsePATCH = ds.doRequest({ urlPath: 'http://localhost:3000/', method: 'PATCH' } as any);
 
     expect(responsePATCH).rejects.toThrowError('Invalid method PATCH');
 
-    const responseDELETE = ds.doRequest({ method: 'DELETE' } as any);
+    const responseDELETE = ds.doRequest({ urlPath: 'http://localhost:3000/', method: 'DELETE' } as any);
 
     expect(responseDELETE).rejects.toThrowError('Invalid method DELETE');
   });
